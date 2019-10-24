a591265e9ddcce4b47752ce7e15309dece35a4f5
Zachary Murray <89foxlx@gmail.com>
2019-10-24 18:36:25 -0500

    quick commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./App.css";
 
+
+//api key - rq6CnqdCTwMdnIzdacBCm3J2Cf65dFWV8eEhZOc6
+// Account Email: 89foxlx@gmail.com
+// Account ID: bbcc3c07-b71e-4da8-ac6d-d36b53180c49
+import PhotoCard from "./components/PhotoCard";
+import PhotoList from "./components/NasaPhotos";
+
 function App() {
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
-      </p>
+      </p> */}
+      {/* <PhotoList/> */}
+      <PhotoCard/>
     </div>
   );
 }
